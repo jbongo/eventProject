dfa25a28e588c4f1796fa043012140f2bbc6e0bd
jbongo <bongojeanphilippe@gmail.com>
2017-12-05 15:18:03 +0100

    Sup fichiers inutiles

--- admin/forgot-password.php
+++ admin/forgot-password.php
@@ -19,28 +19,21 @@
 <body class="bg-dark">
   <div class="container">
     <div class="card card-login mx-auto mt-5">
-      <div class="card-header">Login</div>
+      <div class="card-header">Retrouver mon mot de passe</div>
       <div class="card-body">
+        <div class="text-center mt-4 mb-5">
+          <h4>Mot de passe oublié ?</h4>
+          <p>Entrez votre adresse email et nous vous enverrons votre mot de passe par mail </p>
+        </div>
         <form>
           <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input class="form-control" id="exampleInputEmail1" type="email" aria-describedby="emailHelp" placeholder="Enter email">
+            <input class="form-control" id="exampleInputEmail1" type="email" aria-describedby="emailHelp" placeholder="adresse email">
           </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input class="form-control" id="exampleInputPassword1" type="password" placeholder="Password">
-          </div>
-          <div class="form-group">
-            <div class="form-check">
-              <label class="form-check-label">
-                <input class="form-check-input" type="checkbox"> Remember Password</label>
-            </div>
-          </div>
-          <a class="btn btn-primary btn-block" href="index.html">Login</a>
+          <a class="btn btn-primary btn-block" href="login.html">Envoyer</a>
         </form>
         <div class="text-center">
-          <a class="d-block small mt-3" href="register.html">Register an Account</a>
-          <a class="d-block small" href="forgot-password.html">Forgot Password?</a>
+          <a class="d-block small mt-3" href="register.html">Créer un compte</a>
+          <a class="d-block small" href="login.html">Se connecter</a>
         </div>
       </div>
     </div>
